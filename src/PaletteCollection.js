3331cda09c952f9e592622373d51c1711f5c4b5d
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-04-15 23:52:40 +0545

    Adding Route for palette

--- src/PaletteCollection.js
+++ src/PaletteCollection.js
@@ -10,16 +10,33 @@ export default class PaletteCollection extends Component {
   render() {
     const palette = this.props.paletteArray.map((palette) => {
       return (
-        <div className="card" style={{ width: '18rem' }}>
-          <div className="card-body">
-            <p className="card-text"></p>
+        <div className="card col-md-3">
+          <div className="card-body cardBody">
+            <div className="paletteColors row">
+              {palette.colors.map((color) => {
+                return (
+                  <div
+                    className="eachColor col-sm-3"
+                    style={{
+                      backgroundColor: color.color,
+                      width: '3rem',
+                      height: '2.7rem',
+                    }}
+                  ></div>
+                )
+              })}
+            </div>
+            <div className="paletteBrief">
+              <span className="card-text">{palette.paletteName}</span>
+              <span className="card-text">{palette.emoji}</span>
+            </div>
           </div>
         </div>
       )
     })
     return (
-      <div className="container PaletteCollection">
-        <h1>{palette}</h1>
+      <div className="container PaletteCollection ">
+        <div className="row justify-content-center">{palette}</div>
       </div>
     )
   }
